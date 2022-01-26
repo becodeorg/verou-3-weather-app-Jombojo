68bd1080c01504bd4bdacf9b6c5b980bbb5151f6
Dante-Cand <cand.dante@gmail.com>
2022-01-26 10:51:05 +0100

    cleaner api

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-const cityName2 = document.querySelector("#citySearch");
-const weatherKey = "f20b3d81afbc226e2cf6912fe5397493";
-let cityName 
-document.getElementById("citybutton").addEventListener('click', function(){
-   cityName = document.getElementById("citySearch")
-   fetch('http://api.openweathermap.org/data/2.5/weather?q='+cityName+'&appid='+weatherKey)
-  .then(response => response.json())
-  .then(data => console.log(data));
+let api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+let city;
+let units = '&units=metric&appid=';
+let weatherKey = 'f20b3d81afbc226e2cf6912fe5397493';
+
+const submitbutton = document.querySelector('#submitbutton');
+
+submitbutton.addEventListener('click', () => {
+  let cityName = document.getElementById("citySearch");
+  city = cityName.value;
+  console.log(city);
+  console.log(cityName);
+  const fullApi = api+city+units+weatherKey
+  fetch(fullApi)
+    .then(response => response.json())
+    .then(data => console.log(data));
+    console.log(weather.main.humidity)
+});
+
+
 
 import Data from "./config.js";
-})
-
-
 
